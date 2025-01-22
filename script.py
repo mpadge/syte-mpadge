33e5d56e9818e388cdcfcf3d9b9b700986899feb
mpadge <mark.padgham@email.com>
2025-01-22 11:16:45 +0100

    add buffer dist param and return integer indices into file names

--- script.py
+++ script.py
@@ -4,9 +4,22 @@ import shapely
 import geopandas as gpd
 import pyproj
 
-def pt_to_25832Range(lat: float, lon: float) -> pd.Series:
+def pt_to_25832Range(lat: float, lon: float, buffer_dist: float = 100) -> pd.Series:
     pt = shapely.Point([lon, lat])
     pt = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=[pt])
-    # Re-project to "cea" = "Equal Area Cylindrical" and buffer:
-    pt = pt.to_crs('+proj=cea').buffer(100).to_crs('25832')
-    return pt.bounds.iloc[0]
+    # Re-project to "cea" = "Equal Area Cylindrical" which has
+    # "unit_name=metre" (`.crs.axis_info`), and buffer:
+    pt = pt.to_crs('+proj=cea').buffer(buffer_dist).to_crs('25832')
+    bounds = pt.bounds.iloc[0]
+
+    minx = bounds.loc['minx'].item()
+    miny = bounds.loc['miny'].item()
+    maxx = bounds.loc['maxx'].item()
+    maxy = bounds.loc['maxy'].item()
+
+    minx_i = int (minx // 1000)
+    miny_i = int (miny // 1000)
+    maxx_i = int (maxx // 1000 + 1)
+    maxy_i = int (maxy // 1000 + 1)
+
+    return [minx_i, miny_i, maxx_i, maxy_i]
